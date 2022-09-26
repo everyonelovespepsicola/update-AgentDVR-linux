6d28579befed14b86dfff6dd0392ff22eff95adf
everyonelovespepsicola <40620033+everyonelovespepsicola@users.noreply.github.com>
2022-09-26 02:21:06 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,20 +1,7 @@
 
-echo "finding installer for $(arch)"
-purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=Linux"
 
-
-if [[ ("$OSTYPE" == "darwin"*) ]]; then
   purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=OSX"
-else
-  case $(arch) in
-    'aarch64' | 'arm64')
-      purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=ARM"
-    ;;
-    'arm' | 'armv6l' | 'armv7l')
-      purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=false&platform=ARM32"
-    ;;
-  esac
-fi
+
 
 AGENTURL=$(curl -s --fail "$purl" | tr -d '"')
 Filename=${AGENTURL*/}
